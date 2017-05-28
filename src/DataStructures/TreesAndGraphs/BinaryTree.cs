65b4e1bcc17784106477fed440171938397e73c7
Maycon Beserra <mayconbeserra@hotmail.com>
2017-05-28 20:57:25 +0200

    Add binary search tree

---
+++ src/DataStructures/TreesAndGraphs/BinaryTree.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Learning.DataStructures.TreesAndGraphs
+{
+    public class BinaryTree<T>
+    {
+        public BinaryTreeNode<T> Root { get; set; }
+    }
+
+    public class BinaryTreeNode<T>
+    {
+        public T Value { get; set; }
+        public BinaryTreeNode<T> Parent { get; protected set; }
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Right { get; set; }
+        public int Size { get; protected set; }
+
+        public BinaryTreeNode(T value)
+        {
+            this.Value = value;
+        }
+
+        public void Insert(T newValue)
+        {
+            // TODO fix this comparison to allow generic type
+            if (Convert.ToInt32(newValue) <= Convert.ToInt32(Value))
+            {
+                if (Left == null)
+                {
+                    SetLeftChild(new BinaryTreeNode<T>(newValue));
+                } else 
+                {
+                    Left.Insert(newValue);
+                }
+            } else 
+            {
+                if (Right == null)
+                {
+                    SetRightChild(new BinaryTreeNode<T>(newValue));
+                } 
+                else
+                {
+                    Right.Insert(newValue);
+                }
+            }
+
+            Size++;
+        }
+
+        public BinaryTreeNode<T> Contains(int expected)
+        {
+            int currentValue = Convert.ToInt32(Value);
+
+            if (expected == currentValue) return this;
+            else if (expected <= currentValue)
+                return Left != null ? Left.Contains(expected) : null;
+            else if (expected > currentValue)
+                return Right != null ? Right.Contains(expected) : null;
+
+            return null;
+        }
+
+        public void PrintInOrder() 
+        {
+            if (Left != null) Left.PrintInOrder();
+
+            Console.WriteLine(this.Value);
+
+            if (Right != null) Right.PrintInOrder();
+        }
+
+        public void PreOrder() 
+        {
+            Console.WriteLine(this.Value);
+
+            if (Left != null) Left.PrintInOrder();
+            if (Right != null) Right.PrintInOrder();
+        }
+
+        public void PostOrder() 
+        {
+            if (Left != null) Left.PrintInOrder();
+            if (Right != null) Right.PrintInOrder();
+
+            Console.WriteLine(this.Value);            
+        }
+
+        private void SetLeftChild(BinaryTreeNode<T> binaryTreeNode)
+        {
+            this.Left = binaryTreeNode;
+            if (Left != null) Left.Parent = this;
+        }
+
+        private void SetRightChild(BinaryTreeNode<T> binaryTreeNode)
+        {
+            this.Right = binaryTreeNode;
+            if (Right != null) Right.Parent = this;
+        }
+    }
+}
---
+++ tests/LearningTests/TreesAndGraphs/BinaryTreeTests.cs
@@ -0,0 +1,54 @@
+using System;
+using Xunit;
+using FluentAssertions;
+using Learning.DataStructures.TreesAndGraphs;
+
+namespace Learning.DataStructures.TreesAndGraphs.Tests
+{
+    public class BinaryTreeTests
+    {
+        [Fact]
+        public void It_Should_Add_Node()
+        {
+            BinaryTree<int> tree = new BinaryTree<int>();
+
+            tree.Root = new BinaryTreeNode<int>(10);
+
+            tree.Root.Insert(20);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 3, 2, 4})]
+        public void It_Should_Add_Several_Node(int[] values)
+        {
+            BinaryTree<int> tree = new BinaryTree<int>();
+
+            tree.Root = new BinaryTreeNode<int>(5);
+
+            foreach(var val in values) tree.Root.Insert(val);
+        }
+
+        [Theory]
+        //[InlineData(new int[] { 3, 2, 4}, 2)]
+        [InlineData(new int[] { 3, 2, 50, 20, 30, 1 }, 1)]
+        public void It_Should_FindOne_Node(int[] values, int expected)
+        {
+            BinaryTree<int> tree = new BinaryTree<int>();
+
+            tree.Root = new BinaryTreeNode<int>(5);
+
+            foreach(var val in values) tree.Root.Insert(val);
+
+            tree.Root.Contains(expected).Value.Should().Be(expected);
+
+            Console.WriteLine("###########");
+            tree.Root.PrintInOrder();
+
+            Console.WriteLine("###########");
+            tree.Root.PreOrder();
+
+            Console.WriteLine("###########");
+            tree.Root.PostOrder();
+        }
+    }
+}
